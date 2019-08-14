946ddce1da04f71a5a7dddb45f9ac0668861b165
Ludwig <blacklightinglr@gmail.com>
2019-08-14 00:47:46 -0400

    force en

--- lib/src/api_base_caller.dart
+++ lib/src/api_base_caller.dart
@@ -1,9 +1,6 @@
 import 'dart:convert';
-import 'dart:io';
 
 import 'package:http/http.dart' as http;
-
-import 'exceptions.dart';
 
 class ApiBase {
   ApiBase(this.client) : assert(client != null);
@@ -13,28 +10,14 @@ class ApiBase {
   static const String _baseUrl = 'https://api.warframestat.us';
 
   Future<dynamic> get(String path) async {
-    try {
-      final request = http.Request('GET', Uri.parse(_baseUrl + path));
-      final response = await client.send(request);
+    final response =
+        await http.get(_baseUrl + path, headers: {'Accept-Language': 'en'});
 
-      return _returnResponse(response);
-    } on SocketException {
-      throw Exception();
+    if (response.statusCode != 200) {
+      throw Exception(
+          response?.statusCode ?? 'Error connecting to api.warframestat.us');
     }
-  }
 
-  Future<dynamic> _returnResponse(http.StreamedResponse response) async {
-    switch (response.statusCode) {
-      case 200:
-        return json.decode(await response.stream.bytesToString());
-      case 400:
-        throw BadRequestException(await response.stream.bytesToString());
-      case 403:
-        throw UnauthorisedException(response.statusCode);
-      case 500:
-      default:
-        throw FetchDataException(
-            'Error occured while Communication with Server with StatusCode : ${response.statusCode}');
-    }
+    return json.decode(await response.body);
   }
 }

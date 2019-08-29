caa7cb17f3924552d826271aed6baa033bbb7a5a
Ludwig <blacklightinglr@gmail.com>
2019-08-29 12:11:30 -0400

    decode ItemObjects in another isolate

--- lib/worldstate_wrapper.dart
+++ lib/worldstate_wrapper.dart
@@ -1,6 +1,7 @@
 library worldstate_wrapper;
 
 import 'dart:convert';
+import 'dart:isolate';
 
 import 'package:http/http.dart' as http;
 import 'package:warframe_items_model/warframe_items_model.dart';
@@ -25,23 +26,21 @@ class WorldstateApiWrapper {
   }
 
   Future<List<ItemObject>> searchItems(String searchTerm) async {
+    final recievePort = ReceivePort();
     final response = await _get('items/search/${searchTerm.toLowerCase()}')
       ..cast<Map<String, dynamic>>();
 
-    return response.map<ItemObject>((i) {
-      if (i['category'] == 'Warframe' ||
-          i['category'] == 'Archwing' && !i.containsKey('damage')) {
-        return Warframe.fromJson(i);
-      }
+    await Isolate.spawn(_parseSearchItems, recievePort.sendPort);
 
-      if (i['category'] == 'Primary' ||
-          i['category'] == 'Secondry' ||
-          i['category'] == 'Melee') {
-        return Weapon.fromJson(i);
-      }
+    final sendPort = await recievePort.first;
 
-      return BasicItem.fromJson(i);
-    }).toList();
+    return await sendRecieve(sendPort, response);
+  }
+
+  Future sendRecieve(SendPort port, dynamic data) async {
+    final ReceivePort response = ReceivePort();
+    port.send([data, response.sendPort]);
+    return await response.first;
   }
 
   Future<dynamic> _get(String path, {String lang}) async {
@@ -61,5 +60,30 @@ class WorldstateApiWrapper {
     }
 
     return json.decode(await response.body);
+  }
+}
+
+Future<void> _parseSearchItems(SendPort sendPort) async {
+  final port = ReceivePort();
+
+  sendPort.send(port.sendPort);
+
+  await for (dynamic data in port) {
+    final searchItems = data[0].map<ItemObject>((i) {
+      if (i['category'] == 'Warframe' ||
+          i['category'] == 'Archwing' && !i.containsKey('damage')) {
+        return Warframe.fromJson(i);
+      }
+
+      if (i['category'] == 'Primary' ||
+          i['category'] == 'Secondry' ||
+          i['category'] == 'Melee') {
+        return Weapon.fromJson(i);
+      }
+
+      return BasicItem.fromJson(i);
+    }).toList();
+
+    (data[1] as SendPort).send(searchItems);
   }
 }

3adbf8ca6e631ccb8beed966d76a5446b8c75fc5
SlayerOrnstein <blacklightinglr@gmail.com>
2020-10-25 06:33:29 -0400

    feat: parse new gun and melee models

--- lib/src/utils/json_utils.dart
+++ lib/src/utils/json_utils.dart
@@ -1,5 +1,10 @@
 import 'package:warframestat_api_models/warframestat_api_models.dart';
 
+final _gunReg = RegExp(r'(Primary)|(Secondary)|(Arch-Gun)');
+final _meleeReg = RegExp(r'(Melee)|(Arch-Melee)');
+final _frameReg = RegExp(r'(Warframes)|(Archwings)|(Sentinels)|(Pets)');
+const _mods = 'Mods';
+
 /// Converts a json decoded list into [BaseItem] objects
 List<BaseItem> toBaseItems(List<dynamic> data) {
   return data.map<BaseItem>((dynamic i) {
@@ -33,71 +38,36 @@ List<SynthTarget> toSynthTargets(List<dynamic> data) {
 }
 
 /// Serializes giving json values into their proper [BaseItem] type
-
-// Only reason this is being ignored is becasue the switch statement works fine
-// but it's being flagged as not returning ItemObject anyways
-// ignore: missing_return
 BaseItem toBaseItem(Map<String, dynamic> item) {
-  switch (item['category'] as String) {
-    case 'Archwings':
-      continue warframe;
-    case 'Pets':
-      continue warframe;
-    case 'Sentinels':
-      continue warframe;
+  final category = item['category'] as String;
 
-    case 'Secondary':
-      continue weapon;
-    case 'Melee':
-      continue weapon;
-    case 'Arch-Gun':
-      continue weapon;
-    case 'Arch-Melee':
-      continue weapon;
-
-    warframe:
-    case 'Warframes':
-      return BioWeaponModel.fromJson(item);
-    weapon:
-    case 'Primary':
-      return WeaponModel.fromJson(item);
-    case 'Mods':
-      return ModModel.fromJson(item);
-    default:
-      return BaseItemModel.fromJson(item);
+  if (category.contains(_gunReg)) {
+    return GunModel.fromJson(item);
+  } else if (category.contains(_meleeReg)) {
+    return MeleeModel.fromjson(item);
+  } else if (category.contains(_frameReg)) {
+    return BioWeaponModel.fromJson(item);
+  } else if (category.contains(_mods)) {
+    return ModModel.fromJson(item);
+  } else {
+    return BaseItemModel.fromJson(item);
   }
 }
 
 /// Serializes the appropriate [BaseItem] into a [Map<String, dynamic>]
-// ignore: missing_return
 BaseItem fromBaseItem(BaseItem item) {
-  switch (item.category) {
-    case 'Archwings':
-      continue warframe;
-    case 'Pets':
-      continue warframe;
-    case 'Sentinels':
-      continue warframe;
+  final category = item.category;
 
-    case 'Secondary':
-      continue weapon;
-    case 'Melee':
-      continue weapon;
-    case 'Arch-Gun':
-      continue weapon;
-    case 'Arch-Melee':
-      continue weapon;
-
-    warframe:
-    case 'Warframes':
-      return item as BioWeaponModel;
-    weapon:
-    case 'Primary':
-      return item as WeaponModel;
-    case 'Mods':
-      return item as ModModel;
-    default:
-      return item as BaseItemModel;
+  if (category.contains(_gunReg)) {
+    return item as Gun;
+  } else if (category.contains(_meleeReg)) {
+    return item as Melee;
+  } else if (category.contains(_frameReg)) {
+    return item as BioWeapon;
+  } else if (category.contains(_mods)) {
+    return item as Mod;
+  } else {
+    return item;
   }
 }
 

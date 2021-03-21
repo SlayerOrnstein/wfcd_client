b878f460a7d0f534373e70d3a0e453408694b763
SlayerOrnstein <blacklightinglr@gmail.com>
2021-03-21 14:35:32 -0400

    fix: update steel path entities

--- lib/src/models/worldstate/steel_path_model.g.dart
+++ lib/src/models/worldstate/steel_path_model.g.dart
@@ -13,9 +13,18 @@ SteelPathMdoel _$SteelPathMdoelFromJson(Map json) {
           json, 'activation', (v) => DateTime.parse(v as String)),
       expiry:
           $checkedConvert(json, 'expiry', (v) => DateTime.parse(v as String)),
-      currentReward: $checkedConvert(json, 'currentReward', (v) => v as String),
-      rotation: $checkedConvert(json, 'rotation',
-          (v) => (v as List<dynamic>).map((e) => e as String).toList()),
+      currentReward: $checkedConvert(
+          json,
+          'currentReward',
+          (v) => SteelPathRewardModel.fromJson(
+              Map<String, dynamic>.from(v as Map))),
+      rotation: $checkedConvert(
+          json,
+          'rotation',
+          (v) => (v as List<dynamic>)
+              .map((e) => SteelPathRewardModel.fromJson(
+                  Map<String, dynamic>.from(e as Map)))
+              .toList()),
     );
     return val;
   });
@@ -25,6 +34,23 @@ Map<String, dynamic> _$SteelPathMdoelToJson(SteelPathMdoel instance) =>
     <String, dynamic>{
       'activation': instance.activation?.toIso8601String(),
       'expiry': instance.expiry?.toIso8601String(),
-      'currentReward': instance.currentReward,
-      'rotation': instance.rotation,
+      'currentReward': instance.currentReward.toJson(),
+      'rotation': instance.rotation.map((e) => e.toJson()).toList(),
+    };
+
+SteelPathRewardModel _$SteelPathRewardModelFromJson(Map json) {
+  return $checkedNew('SteelPathRewardModel', json, () {
+    final val = SteelPathRewardModel(
+      name: $checkedConvert(json, 'name', (v) => v as String),
+      cost: $checkedConvert(json, 'cost', (v) => v as int),
+    );
+    return val;
+  });
+}
+
+Map<String, dynamic> _$SteelPathRewardModelToJson(
+        SteelPathRewardModel instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+      'cost': instance.cost,
     };
